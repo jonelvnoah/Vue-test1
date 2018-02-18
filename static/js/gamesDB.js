5607e3c99cfc1623e6a386b5be82b6fbdce8b16b
jonelvnoah <jonelvnoah@gmail.com>
2018-02-18 02:46:03 -0500

    first VueJs Test

---
+++ static/js/gamesDB.js
@@ -0,0 +1,59 @@
+let games = [
+    {
+        "id"       : 0,
+        "name"     : "神秘海域123",
+        "img"      : "./static/images/uncharted-collection.jpg",
+        "price"    : 160.80,
+        "purchased": false
+    },
+    {
+        "id"       : 1,
+        "name"     : "神秘海域4",
+        "img"      : "./static/images/uncharted4.jpg",
+        "price"    : 318.40,
+        "purchased": false
+    },
+    {
+        "id"       : 2,
+        "name"     : "合金装备 幻痛",
+        "img"      : "./static/images/metal-gear-solid.jpg",
+        "price"    : 179.00,
+        "purchased": false
+    },
+    {
+        "id"       : 3,
+        "name"     : "刺客信条 枭雄",
+        "img"      : "./static/images/assassins.jpg",
+        "price"    : 118.80,
+        "purchased": false
+    },
+    {
+        "id"       : 4,
+        "name"     : "GTA5",
+        "img"      : "./static/images/gta5.jpg",
+        "price"    : 234.00,
+        "purchased": false
+    },
+    {
+        "id"       : 5,
+        "name"     : "看门狗",
+        "img"      : "./static/images/watch-dogs.jpg",
+        "price"    : 69.30,
+        "purchased": false
+    },
+    {
+        "id"       : 6,
+        "name"     : "看门狗2",
+        "img"      : "./static/images/watch-dogs2.jpg",
+        "price"    : 420.00,
+        "purchased": false
+    },
+
+    {
+        "id"       : 7,
+        "name"     : "热血无赖",
+        "img"      : "./static/images/sleeping-dogs.jpg",
+        "price"    : 198,
+        "purchased": false
+    }
+]
